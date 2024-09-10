69cdcbf315543c599796116faea1fbe120e82b3c
ismaeel-m <87246030+ismaeel-m@users.noreply.github.com>
2024-09-10 08:57:07 -0400

    Add computer choice function

--- game.js
+++ game.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+let rock = 0;
+let paper = 1;
+let scissors = 2;
+
+function getComputerChoice(){
+    randomNumber = Math.floor(Math.random() * 3);
+
+    switch (randomNumber){
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
