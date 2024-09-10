38743e978ace979986dbc7f2526ca3528739abc3
ismaeel-m <87246030+ismaeel-m@users.noreply.github.com>
2024-09-10 13:27:55 -0400

    Add function to play round

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 let rock = 0;
 let paper = 1;
 let scissors = 2;
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 3);
@@ -22,5 +24,28 @@ function getHumanChoice(){
 }
 
 
-console.log(getHumanChoice());
-console.log(getComputerChoice());
+function playRound(humanChoice, computerChoice){
+
+    humanChoice = humanChoice.toLowerCase();
+    console.log(humanChoice);
+    console.log(computerChoice);
+    if(humanChoice == computerChoice){
+        console.log("It's a tie! No point awarded.");
+    } else if (
+        (humanChoice == "rock" && computerChoice == "scissors") || 
+        (humanChoice == "paper" && computerChoice == "rock") || 
+        (humanChoice == "scissors" && computerChoice == "paper")
+    ){
+        console.log("You win! " + humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1) + " beats " + computerChoice + "!");
+        humanScore++;
+    } else{
+        console.log("You lose! " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1) + " beats " + humanChoice + "!");
+        computerScore++;
+    }
+}
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
